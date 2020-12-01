952d93cb3761846949360192038c2cb151c621de
Gentrit Selimi <gentrit.selimi@riinvest.net>
2020-12-01 16:36:49 +0100

    Update!

---
+++ GameCenterMVC/Content/Scripts/DeleteNotify.js
@@ -0,0 +1,19 @@
+﻿@section Scripts{
+
+
+    <script>
+        $(function () {
+        var SuccessMessage = '@TempData["SuccessMessage"]'
+        if (SuccessMessage != '')
+            alertify.success(SuccessMessage);
+
+    });
+
+    function Delete(id) {
+            alertify.confirm('customer Management System', 'Are You Sure To Delete This Record ?', function () {
+                window.location.href = '@Url.Action("Delete","Employee")/' + id;
+            }, null);
+    }
+	</script>
+
+} 
---
+++ GameCenterMVC/Controllers/BaseController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameCenterMVC.Controllers
+{
+    public class BaseController : Controller
+    {
+		// GET: Base
+		protected override void OnActionExecuting(ActionExecutingContext filterContext)
+		{
+			if(filterContext.HttpContext.Session != null)
+			{
+				if (this.Session["Username"] == null)
+				{
+					filterContext.Result = RedirectToAction("Login", "Account");
+					return;
+
+				}
+			}
+			base.OnActionExecuting(filterContext);
+		}
+
+
+	}
+}
